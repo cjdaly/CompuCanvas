15ee45648f21e26253b7b35ef912d2e8105c1aaa
cjdaly <cjdaly@locosoft.com>
2016-04-16 17:17:59 -0700

    setup tweaks

--- code/sudo-setup.sh
+++ code/sudo-setup.sh
@@ -10,14 +10,21 @@
 #   cjdaly - initial API and implementation
 ####
 
+COMPUCANVAS_HOME=/home/pi/CompuCanvas
+
 apt-get update
 apt-get upgrade -y
 apt-get install espeak mpg321 -y
 pip install blinkstick
 blinkstick --add-udev-rule
 
+if [ ! -f /etc/modprobe.d/alsa-base.conf ]; then
+  echo "Copying alsa-base.conf to /etc/modprobe.d ..."
+  cp "$COMPUCANVAS_HOME/code/alsa-base.conf" /etc/modprobe.d
+fi
+
 if ! grep "boot-sequence" /etc/rc.local
 then
-  echo "Adding boot-sequence script to /etc/rc.local..."
-  sed -i -e "s/^exit 0$/\/home\/pi\/CompuCanvas\/code\/boot-sequence.sh\n\nexit 0/" /etc/rc.local
+  echo "Adding boot-sequence script to /etc/rc.local ..."
+  sed -i -e "s:^exit 0$:$COMPUCANVAS_HOME/code/boot-sequence.sh\n\nexit 0:" /etc/rc.local
 fi
